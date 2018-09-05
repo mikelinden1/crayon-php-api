1c24f7067087a9c684ded9fb7490e897bd54960c
mikelinden1 <mike@mikelinden.com>
2018-09-05 16:33:01 -0400

    added client and path to db conn

--- utils/database-connection-sample.php
+++ utils/database-connection-sample.php
@@ -1,6 +1,14 @@
 <?php
+global $db_host;
+global $db_user;
+global $db_pass;
+global $db_name;
+
 $db_host = 'HOST';
 $db_user = 'USER';
 $db_pass = 'PASSWORD';
 $db_name = 'DB_NAME';
+
+$clientName = 'CLIENT NAME';
+$path = '';
 ?>
