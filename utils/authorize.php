f9bc5b121366fa869416b9e3b885965f5c721f25
Mike Linden <mike@mikelinden.com>
2025-07-15 14:34:36 -0400

    fix auth

--- utils/authorize.php
+++ utils/authorize.php
@@ -2,6 +2,20 @@
 require_once('vendor/autoload.php');
 use \Firebase\JWT\JWT;
 
+if (!function_exists('getallheaders')) {
+    function getallheaders()
+    {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, strlen('HTTP_')) === 'HTTP_') {
+                $headerName = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))));
+                $headers[$headerName] = $value;
+            }
+        }
+        return $headers;
+    }
+}
+
 $authorized_user = has_api_access();
 
 if (!$authorized_user) {
@@ -13,7 +27,11 @@ function has_api_access() {
     $headers = getallheaders();
     $jwt = !empty($headers['Authorization']) ? $headers['Authorization'] : $headers['authorization'];
     
-    $jwt = str_replace('Bearer ', '', $jwt);
+    if ($jwt) {
+        $jwt = str_replace('Bearer ', '', $jwt);
+    } else if (isset($_COOKIE['usr'])) {
+        $jwt = $_COOKIE['usr'];
+    }
 
     if ($jwt) {
         try {
@@ -22,7 +40,6 @@ function has_api_access() {
 
             // decode the jwt into a user
             $user = JWT::decode($jwt, $jwt_secret_key, array($jwt_hashing_algorithm));
-
             return $user;
         } catch(Exception $e) {
             header('HTTP/1.0 401 Unauthorized');
